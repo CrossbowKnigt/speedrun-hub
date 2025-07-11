e7c66e4540f2a0326915f58a609f678ae938836d
Crossbow <jaimefa00@gmail.com>
2025-07-11 19:15:43 +0200

    build: initialize backend project

---
+++ backend/src/main/java/com/cross/speedrunhub/SpeedrunhubApplication.java
@@ -0,0 +1,13 @@
+package com.cross.speedrunhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedrunhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedrunhubApplication.class, args);
+	}
+
+}
